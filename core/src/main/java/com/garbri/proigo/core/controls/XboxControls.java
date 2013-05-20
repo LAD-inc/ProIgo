2031aae8f4a1582e1057eb33054380b3681b35e7
Conor Fennell <fennell.conor@gmail.com>
2013-05-20 23:51:32 +0100

    Remove Cont since it is not used anymore and removed reference to it in the XboxControls

--- core/src/main/java/com/garbri/proigo/core/controls/XboxControls.java
+++ core/src/main/java/com/garbri/proigo/core/controls/XboxControls.java
@@ -31,51 +31,5 @@ public class XboxControls implements IControls{
 	public void setAccelerate(Boolean accelerate) {
 		this.accelerate = accelerate;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((accelerate == null) ? 0 : accelerate.hashCode());
-		result = prime * result + ((brake == null) ? 0 : brake.hashCode());
-		result = prime * result + ((left == null) ? 0 : left.hashCode());
-		result = prime * result + ((right == null) ? 0 : right.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cont other = (Cont) obj;
-		if (accelerate == null) {
-			if (other.accelerate != null)
-				return false;
-		} else if (!accelerate.equals(other.accelerate))
-			return false;
-		if (brake == null) {
-			if (other.brake != null)
-				return false;
-		} else if (!brake.equals(other.brake))
-			return false;
-		if (left == null) {
-			if (other.left != null)
-				return false;
-		} else if (!left.equals(other.left))
-			return false;
-		if (right == null) {
-			if (other.right != null)
-				return false;
-		} else if (!right.equals(other.right))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Cont [left=" + left + ", right=" + right + ", accelerate="
-				+ accelerate + ", brake=" + brake + "]";
-	}
+
 }
